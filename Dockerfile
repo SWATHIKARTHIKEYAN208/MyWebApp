06b80d43535fc27f64d1d5dab0d3660eafa3bf75
SWATHIKARTHIKEYAN208 <shreeswathi1996@gmail.com>
2023-10-14 16:41:37 +0530

    AppCommit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM eclipse-temurin:17
+COPY target/mywebapp.jar mywebapp.jar
+CMD ["java","-jar","mywebapp.jar"]
+
---
+++ src/main/java/com/example/end/MywebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebAppApplication.class, args);
+	}
+
+}
